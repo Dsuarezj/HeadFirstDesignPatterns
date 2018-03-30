21b7964c25d3d3771d072265d80799dfed957a93
Denisse Suarez <dsuarez@thoughtworks.com>
2018-03-30 09:58:46 -0500

    Create a Model Duck

---
+++ src/com/training/domain/ModelDuck.java
@@ -0,0 +1,15 @@
+package com.training.domain;
+
+public class ModelDuck extends Duck {
+    public String swim() {
+        return "All ducks float";
+    }
+
+    public String quack() {
+        return "Squeak";
+    }
+
+    public String fly() {
+        return "I can not fly";
+    }
+}
---
+++ test/com/training/domain/ModelDuckTest.java
@@ -0,0 +1,45 @@
+package com.training.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ModelDuckTest {
+    ModelDuck duck;
+    String exceptedMessage;
+
+    @Before
+    public void setUp() {
+        duck = new ModelDuck();
+    }
+
+    @Test
+    public void shouldReturnAllDuckFloatWhenPreformingASwimming() {
+        exceptedMessage = "All ducks float";
+
+        String swimMessage = duck.swim();
+
+        assertThat(swimMessage, is(exceptedMessage));
+    }
+
+    @Test
+    public void shouldReturnARealQuackWhenPreformingAQuack() {
+        exceptedMessage = "Squeak";
+
+        String quackMessage = duck.quack();
+
+        assertThat(quackMessage, is(exceptedMessage));
+    }
+
+    @Test
+    public void shouldReturnAFlyingTypeWhenPreformingAFly() {
+        exceptedMessage = "I can not fly";
+
+        String flyMessage = duck.fly();
+
+        assertThat(flyMessage, is(exceptedMessage));
+    }
+
+}
